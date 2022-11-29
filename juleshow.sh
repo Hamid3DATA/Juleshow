40d1b7ead5e6bb845369502c2363ac77c750b495
Hamid <118181996+Hamid3DATA@users.noreply.github.com>
2022-11-29 14:06:34 +0100

    Delete juleshow.sh

--- juleshow.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-export DISPLAY=:0
-chromium-browser http://192.168.1.148 --window-size=1280,1024 --start-fullscreen --kiosk --incognito --noerrdialogs --disable-translate --no-first-run --fast --fast-start --disable-infobars --disable-features=TranslateUI --disk-cache-dir=/dev/null  --password-store=basic
