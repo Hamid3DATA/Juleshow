4f787bad92a8f7e94a93a0a5141cbfff74f0752b
Hamid <118181996+Hamid3DATA@users.noreply.github.com>
2022-11-30 09:32:32 +0100

    Update not_installed.sh

--- Automatic Raspberry Pi setup scripts/not_installed.sh
+++ Automatic Raspberry Pi setup scripts/not_installed.sh
@@ -4,13 +4,9 @@ sudo apt install screen -y
 
 sudo apt install unclutter -y
 
-sudo apt install xdotool -y
-
 wget https://raw.githubusercontent.com/Hamid3DATA/Juleshow/main/Raspberry%20Pi%20Files/juleshow.sh && sudo chmod +x juleshow.sh
 wget https://raw.githubusercontent.com/Hamid3DATA/Juleshow/main/Raspberry%20Pi%20Files/cursor.sh && sudo chmod +x cursor.sh
-wget https://raw.githubusercontent.com/Hamid3DATA/Juleshow/main/Raspberry%20Pi%20Files/refreshpage.sh && sudo chmod +x refreshpage.sh
+sleep 1
 { crontab -l -u pi; echo '@reboot sleep 10;(. ~/.profile; /usr/bin/screen -dmS Juleshow ~/juleshow.sh)'; } | crontab -u pi -
 sleep 1
 { crontab -l -u pi; echo '@reboot sleep 15;(. ~/.profile; /usr/bin/screen -dmS Cursor ~/cursor.sh)'; } | crontab -u pi -
-sleep 1
-{ crontab -l -u pi; echo '0 * * * * (. ~/.profile; /usr/bin/screen -dmS RereshPage ~/refreshpage.sh)'; } | crontab -u pi -
