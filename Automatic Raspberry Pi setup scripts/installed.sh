7686e8ad00b4b184437219095fa357ba8a79c85b
Hamid <118181996+Hamid3DATA@users.noreply.github.com>
2022-11-30 12:54:23 +0100

    Update installed.sh

--- Automatic Raspberry Pi setup scripts/installed.sh
+++ Automatic Raspberry Pi setup scripts/installed.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget https://raw.githubusercontent.com/Hamid3DATA/Juleshow/main/Raspberry%20Pi%20Files/julshow.sh +x julshow.sh
+wget https://raw.githubusercontent.com/Hamid3DATA/Juleshow/main/Raspberry%20Pi%20Files/julshow.sh && sudo chmod +x julshow.sh
 sleep 1
 wget https://raw.githubusercontent.com/Hamid3DATA/Juleshow/main/Raspberry%20Pi%20Files/cursor.sh && sudo chmod +x cursor.sh
 sleep 1
